3630b66548e00b10f85fdb803313823fdb2e95cb
Rupa Netha <iamsairupa06@gmail.com>
2026-07-10 20:27:22 +0530

    add probability matrix and sampling

--- bigram.py
+++ bigram.py
@@ -76,20 +76,27 @@ p
 
 torch.multinomial(p, num_samples=100, replacement=True, generator=g)
 
+# Probability Matrix and sampling
+P = N.float()
+P /= P.sum(1, keepdim=True)
+
+
 g = torch.Generator().manual_seed(2147483647)
 
-for i in range(20):
+for i in range(5):
     
     out = []
     ix = 0
     while True:
-        p = N[ix].float()
-        p = p/p.sum()
+        
+        p = P[ix]
         ix = torch.multinomial(p, num_samples=1, replacement=True, generator=g).item()
         out.append(itos[ix])
         if ix == 0:
             break
     print(''.join(out))
+
+
 
 
 
