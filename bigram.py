92644afea64687b62fd791934c03d34ba5cf9356
Rupa Netha <iamsairupa06@gmail.com>
2026-07-11 16:39:57 +0530

    add final optimization

--- bigram.py
+++ bigram.py
@@ -238,7 +238,80 @@ probs[0].sum()
     
     
     
+################################################################################## 
+### OPTIMIZATION 
+################################################################################## 
+  
+xs
+# tensor([ 0,  5, 13, 13,  1])
 
+ys    
+# tensor([ 5, 13, 13,  1,  0])
+
+
+# randomly initialize 27 neurons' weights, each neuron receives 27 inputs
+g = torch.Generator().manual_seed(2147483647)
+W = torch.randn((27,27), generator=g, requires_grad=True)
+  
+
+# forward pass
+xenc = F.one_hot(xs, num_classes=27).float()
+logits = xenc @ W  # predict log-counts
+counts = logits.exp() # counts, equivalent to N
+probs = counts / counts.sum(1, keepdim=True) # probabilities for next character
+loss = -probs[torch.arange(5), ys].log().mean()
+
+print(loss.item())
+
+# backward pass
+W.grad = None # set the gradient to zero
+loss.backward()
+
+W.data += -0.1 * W.grad
+
+
+    
+################################################################################## 
+### FINAL OPTIMIZATION 
+################################################################################## 
+    
+# create the dataset
+xs, ys = [], []
+for w in words:
+    chs = ['.'] + list(w) + ['.']
+    for ch1, ch2 in zip(chs, chs[1:]):
+        ix1 = stoi[ch1]
+        ix2 = stoi[ch2]
+        xs.append(ix1)
+        ys.append(ix2) 
+xs = torch.tensor(xs)
+ys = torch.tensor(ys)
+num = xs.nelement()
+print('number of examples: ', num)
+  
+
+# initialize the 'network'
+g = torch.Generator().manual_seed(2147483647)
+W = torch.randn((27,27), generator=g, requires_grad=True)
+  
+    
+# gradient descent
+for k in range(100):
+    
+    # forward pass
+    xenc = F.one_hot(xs, num_classes=27).float() # input to the network: one-hot encoding
+    logits = xenc @ W  # predict log-counts
+    counts = logits.exp() # counts, equivalent to N
+    probs = counts / counts.sum(1, keepdim=True) # probabilities for next character
+    loss = -probs[torch.arange(num), ys].log().mean()
+    print(loss.item())
+    
+    # backward pass
+    W.grad = None # set the gradient to zero
+    loss.backward()
+    
+    #update 
+    W.data += -50 * W.grad
 
     
     
