04058285c770adbbcef567429553c830afa86ad7
Rupa Netha <iamsairupa06@gmail.com>
2026-07-12 19:13:59 +0530

    re-build our training dataset

--- mlp.py
+++ mlp.py
@@ -27,5 +27,29 @@ itos = {i:s for s,i in stoi.items()}
 print(itos)
 
 
+# -----------------------------------------------------------------------------
+# re-building our training dataset
 
+block_size = 3 # context length: how many characters do we take to predict the next one?
+X, Y = [], []
+for w in words[:5]:
+    
+    print(w)
+    context = [0] * block_size
+    for ch in w + '.':
+        ix = stoi[ch]
+        X.append(context)
+        Y.append(ix)
+        print(''.join(itos[i] for i in context), '--->', itos[ix])
+        context = context[1:] + [ix] # crop and append
+        
+X = torch.tensor(X)
+Y = torch.tensor(Y)
+# output example:
+# emma
+# ... ---> e
+# ..e ---> m
+# .em ---> m
+# emm ---> a
+# mma ---> .
 
