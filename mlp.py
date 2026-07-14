389ee22c0ecb6005ed009fe1b0ef175905614c7b
Rupa Netha <iamsairupa06@gmail.com>
2026-07-14 12:01:54 +0530

    final cleanup and sampling

--- mlp.py
+++ mlp.py
@@ -31,75 +31,135 @@ print(itos)
 # re-building our training dataset
 
 block_size = 3 # context length: how many characters do we take to predict the next one?
-X, Y = [], []
-for w in words[:5]:
-    
-    print(w)
+def build_dataset(words):
+  X, Y = [], []
+  for w in words:
+
+    #print(w)
     context = [0] * block_size
     for ch in w + '.':
-        ix = stoi[ch]
-        X.append(context)
-        Y.append(ix)
-        print(''.join(itos[i] for i in context), '--->', itos[ix])
-        context = context[1:] + [ix] # crop and append
-        
-X = torch.tensor(X)
-Y = torch.tensor(Y)
-# output example:
-# emma
-# ... ---> e
-# ..e ---> m
-# .em ---> m
-# emm ---> a
-# mma ---> .
+      ix = stoi[ch]
+      X.append(context)
+      Y.append(ix)
+      #print(''.join(itos[i] for i in context), '--->', itos[ix])
+      context = context[1:] + [ix] # crop and append
+
+  X = torch.tensor(X)
+  Y = torch.tensor(Y)
+  print(X.shape, Y.shape)
+  return X, Y
+
+import random
+random.seed(42)
+random.shuffle(words)
+n1 = int(0.8*len(words))
+n2 = int(0.9*len(words))
+
+Xtr, Ytr = build_dataset(words[:n1])
+Xdev, Ydev = build_dataset(words[n1:n2])
+Xte, Yte = build_dataset(words[n2:])
 
 
-# -----------------------------------------------------------------------------
-# implementing hidden layer and internals of torch.Tensor: storage, views
-
-X.shape, Y.shape # dataset
-# output: (torch.Size([32, 3]), torch.Size([32]))
-
-g = torch.Generator().manual_seed(2147483647)
-C = torch.randn((27, 2), generator=g)
-W1 = torch.randn((6, 100), generator=g)
-b1 = torch.randn(100, generator=g)
-W2 = torch.randn((100, 27), generator=g)
+g = torch.Generator().manual_seed(2147483647) # for reproducibility
+C = torch.randn((27, 10), generator=g)
+W1 = torch.randn((30, 200), generator=g)
+b1 = torch.randn(200, generator=g)
+W2 = torch.randn((200, 27), generator=g)
 b2 = torch.randn(27, generator=g)
 parameters = [C, W1, b1, W2, b2]
 
 sum(p.nelement() for p in parameters) # number of parameters in total
-# 3481
 
+lre = torch.linspace(-3, 0, 1000)
+lrs = 10**lre
+
+lri = []
+lossi = []
+stepi = []
 
 for p in parameters:
     p.requires_grad = True
-
-for _ in range(100):
-    # forward pass
-    emb = C[X] # (32, 3, 2)
-    h = torch.tanh(emb.view(-1, 6) @ W1 + b1) # (32, 100)
-    logits = h @ W2 +b2 # (32, 27)
-    # counts = logits.exp()
-    # prob = counts / counts.sum(1, keepdims = True)
-    # loss = -prob[torch.arange(32), Y].log().mean()
-    loss = F.cross_entropy(logits, Y)
-    loss
-    # tensor(17.7697)
     
-    # backward pass
-    for p in parameters:
-        p.grad = None
-    loss.backward()
+
+for i in range(200000):
+  
+  # minibatch construct
+  ix = torch.randint(0, Xtr.shape[0], (32,))
+  
+  # forward pass
+  emb = C[Xtr[ix]] # (32, 3, 2)
+  h = torch.tanh(emb.view(-1, 30) @ W1 + b1) # (32, 100)
+  logits = h @ W2 + b2 # (32, 27)
+  loss = F.cross_entropy(logits, Ytr[ix])
+  #print(loss.item())
+  
+  # backward pass
+  for p in parameters:
+    p.grad = None
+  loss.backward()
+  
+  # update
+  #lr = lrs[i]
+  lr = 0.1 if i < 100000 else 0.01
+  for p in parameters:
+    p.data += -lr * p.grad
+
+  # track stats
+  #lri.append(lre[i])
+  stepi.append(i)
+  lossi.append(loss.log10().item())
+
+#print(loss.item())
+
+plt.plot(stepi, lossi)
+
+# training loss 
+emb = C[Xtr] # (32, 3, 2)
+h = torch.tanh(emb.view(-1, 30) @ W1 + b1) # (32, 100)
+logits = h @ W2 + b2 # (32, 27)
+loss = F.cross_entropy(logits, Ytr)
+loss
+
+# validation loss
+emb = C[Xdev] # (32, 3, 2)
+h = torch.tanh(emb.view(-1, 30) @ W1 + b1) # (32, 100)
+logits = h @ W2 + b2 # (32, 27)
+loss = F.cross_entropy(logits, Ydev)
+loss
+
+# test loss
+emb = C[Xte] # (32, 3, 2)
+h = torch.tanh(emb.view(-1, 30) @ W1 + b1) # (32, 100)
+logits = h @ W2 + b2 # (32, 27)
+loss = F.cross_entropy(logits, Yte)
+loss
+
+# visualize dimensions 0 and 1 of the embedding matrix C for all characters
+plt.figure(figsize=(8,8))
+plt.scatter(C[:,0].data, C[:,1].data, s=200)
+for i in range(C.shape[0]):
+    plt.text(C[i,0].item(), C[i,1].item(), itos[i], ha="center", va="center", color='white')
+plt.grid('minor')
+
+
+# sample from the model
+g = torch.Generator().manual_seed(2147483647 + 10)
+
+for _ in range(20):
     
-    # update
-    for p in parameters:
-        p.data += -0.1 * p.grad
-print(loss.item())
-
-
-
-
-
+    out = []
+    context = [0] * block_size # initialize with all ...
+    while True:
+      emb = C[torch.tensor([context])] # (1,block_size,d)
+      h = torch.tanh(emb.view(1, -1) @ W1 + b1)
+      logits = h @ W2 + b2
+      probs = F.softmax(logits, dim=1)
+      ix = torch.multinomial(probs, num_samples=1, generator=g).item()
+      context = context[1:] + [ix]
+      out.append(ix)
+      if ix == 0:
+        break
+    
+    print(''.join(itos[i] for i in out))
 
 
