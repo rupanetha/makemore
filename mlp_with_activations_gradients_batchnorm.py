9aa67016bfb7d6d831f41c687cd34b794b9e4d8b
Rupa Netha <iamsairupa06@gmail.com>
2026-07-16 21:05:09 +0530

    add batchnorm1d class

--- mlp_with_activations_gradients_batchnorm.py
+++ mlp_with_activations_gradients_batchnorm.py
@@ -221,8 +221,39 @@ class Linear:
     return [self.weight] + ([] if self.bias is None else [self.bias])
 
 
-
-
+class BatchNorm1d:
+    
+    def __init__(self, dim, eps=1e-5, momentum=0.1):
+        self.eps = eps
+        self.momentum = momentum
+        self.training = True
+        # parameters (trained with backprop)
+        self.gamma = torch.ones(dim)
+        self.beta = torch.zeros(dim)
+        # bufferes (trained with a running 'momentum update')
+        self.running_mean = torch.zeros(dim)
+        self.running_var = torch.ones(dim)
+        
+    def __call__(self, x):
+        # calculate the forward pass
+        if self.training:
+            xmean = x.mean(0, keepdim = True) # batch mean
+            xvar = x.var(0, keepdim = True) # batch variance
+        else:
+            xmean = self.running_mean
+            xvar = self.running_var
+        xhat = (x - xmean) / torch.sqrt(xvar + self.eps) # normalize to unit variance
+        self.out = self.gamma * xhat + self.beta
+        # update the buffers
+        if self.training:
+            with torch.no_grad():
+                self.running_mean = (1 - self.momentum) * self.running_mean + self.momentum * xmean
+                self.running_var = (1 - self.momentum) * self.running_var + self.momentum * xvar
+        return self.out
+    
+    def parameters(self):
+        return [self.gamma, self.beta]
+            
 
 
 
