2e0dab45bfa7e3fb6b792f3cf017615453d7bb90
Rupa Netha <iamsairupa06@gmail.com>
2026-07-22 18:12:05 +0530

    add exercise 2

--- backprop.py
+++ backprop.py
@@ -183,8 +183,26 @@ loss
 
 
 
+#------------------------------------------------------------------------------
+# Exercise 2: backprop through cross_entropy but all in one go
+# to complete this challenge look at the mathematical expression of the loss,
+# take the derivative, simplify the expression, and just write it out
 
+# forward pass
 
+# before:
+# logit_maxes = logits.max(1, keepdim=True).values
+# norm_logits = logits - logit_maxes # subtract max for numerical stability
+# counts = norm_logits.exp()
+# counts_sum = counts.sum(1, keepdims=True)
+# counts_sum_inv = counts_sum**-1 # if I use (1.0 / counts_sum) instead then I can't get backprop to be bit exact...
+# probs = counts * counts_sum_inv
+# logprobs = probs.log()
+# loss = -logprobs[range(n), Yb].mean()
+
+# now:
+loss_fast = F.cross_entropy(logits, Yb)
+print(loss_fast.item(), 'diff:', (loss_fast - loss).item())
 
 
 
