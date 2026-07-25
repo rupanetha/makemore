09d447e4bd3a30d540c436f299f998a64a3d770c
Rupa Netha <iamsairupa06@gmail.com>
2026-07-25 18:33:42 +0530

    add backprop in exercise 1

--- backprop.py
+++ backprop.py
@@ -149,37 +149,68 @@ loss
 # backpropagating through exactly all of the variables 
 # as they are defined in the forward pass above, one by one
 
-
 dlogprobs = torch.zeros_like(logprobs)
 dlogprobs[range(n), Yb] = -1.0/n
-
-# cmp('logprobs', dlogprobs, logprobs)
-# cmp('probs', dprobs, probs)
-# cmp('counts_sum_inv', dcounts_sum_inv, counts_sum_inv)
-# cmp('counts_sum', dcounts_sum, counts_sum)
-# cmp('counts', dcounts, counts)
-# cmp('norm_logits', dnorm_logits, norm_logits)
-# cmp('logit_maxes', dlogit_maxes, logit_maxes)
-# cmp('logits', dlogits, logits)
-# cmp('h', dh, h)
-# cmp('W2', dW2, W2)
-# cmp('b2', db2, b2)
-# cmp('hpreact', dhpreact, hpreact)
-# cmp('bngain', dbngain, bngain)
-# cmp('bnbias', dbnbias, bnbias)
-# cmp('bnraw', dbnraw, bnraw)
-# cmp('bnvar_inv', dbnvar_inv, bnvar_inv)
-# cmp('bnvar', dbnvar, bnvar)
-# cmp('bndiff2', dbndiff2, bndiff2)
-# cmp('bndiff', dbndiff, bndiff)
-# cmp('bnmeani', dbnmeani, bnmeani)
-# cmp('hprebn', dhprebn, hprebn)
-# cmp('embcat', dembcat, embcat)
-# cmp('W1', dW1, W1)
-# cmp('b1', db1, b1)
-# cmp('emb', demb, emb)
-# cmp('C', dC, C)
-
+dprobs = (1.0 / probs) * dlogprobs
+dcounts_sum_inv = (counts * dprobs).sum(1, keepdim=True)
+dcounts = counts_sum_inv * dprobs
+dcounts_sum = (-counts_sum**-2) * dcounts_sum_inv
+dcounts += torch.ones_like(counts) * dcounts_sum
+dnorm_logits = counts * dcounts
+dlogits = dnorm_logits.clone()
+dlogit_maxes = (-dnorm_logits).sum(1, keepdim=True)
+dlogits += F.one_hot(logits.max(1).indices, num_classes=logits.shape[1]) * dlogit_maxes
+dh = dlogits @ W2.T
+dW2 = h.T @ dlogits
+db2 = dlogits.sum(0)
+dhpreact = (1.0 - h**2) * dh
+dbngain = (bnraw * dhpreact).sum(0, keepdim=True)
+dbnraw = bngain * dhpreact
+dbnbias = dhpreact.sum(0, keepdim=True)
+dbndiff = bnvar_inv * dbnraw
+dbnvar_inv = (bndiff * dbnraw).sum(0, keepdim=True)
+dbnvar = (-0.5*(bnvar + 1e-5)**-1.5) * dbnvar_inv
+dbndiff2 = (1.0/(n-1))*torch.ones_like(bndiff2) * dbnvar
+dbndiff += (2*bndiff) * dbndiff2
+dhprebn = dbndiff.clone()
+dbnmeani = (-dbndiff).sum(0)
+dhprebn += 1.0/n * (torch.ones_like(hprebn) * dbnmeani)
+dembcat = dhprebn @ W1.T
+dW1 = embcat.T @ dhprebn
+db1 = dhprebn.sum(0)
+demb = dembcat.view(emb.shape)
+dC = torch.zeros_like(C)
+for k in range(Xb.shape[0]):
+  for j in range(Xb.shape[1]):
+    ix = Xb[k,j]
+    dC[ix] += demb[k,j]
+    
+cmp('logprobs', dlogprobs, logprobs)
+cmp('probs', dprobs, probs)
+cmp('counts_sum_inv', dcounts_sum_inv, counts_sum_inv)
+cmp('counts_sum', dcounts_sum, counts_sum)
+cmp('counts', dcounts, counts)
+cmp('norm_logits', dnorm_logits, norm_logits)
+cmp('logit_maxes', dlogit_maxes, logit_maxes)
+cmp('logits', dlogits, logits)
+cmp('h', dh, h)
+cmp('W2', dW2, W2)
+cmp('b2', db2, b2)
+cmp('hpreact', dhpreact, hpreact)
+cmp('bngain', dbngain, bngain)
+cmp('bnbias', dbnbias, bnbias)
+cmp('bnraw', dbnraw, bnraw)
+cmp('bnvar_inv', dbnvar_inv, bnvar_inv)
+cmp('bnvar', dbnvar, bnvar)
+cmp('bndiff2', dbndiff2, bndiff2)
+cmp('bndiff', dbndiff, bndiff)
+cmp('bnmeani', dbnmeani, bnmeani)
+cmp('hprebn', dhprebn, hprebn)
+cmp('embcat', dembcat, embcat)
+cmp('W1', dW1, W1)
+cmp('b1', db1, b1)
+cmp('emb', demb, emb)
+cmp('C', dC, C)
 
 
 #------------------------------------------------------------------------------
